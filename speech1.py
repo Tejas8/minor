bda1c4e3f1d035c8398ac6b660320cb3e11f79c0
tejas pandey <tejaspandey0498@gmail.com>
2019-10-05 14:22:01 +0530

    initial commit

---
+++ speech1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 28 02:05:21 2019
+
+@author: tejas
+"""
+
+from python_speech_features import mfcc
+from python_speech_features import logfbank
+import scipy.io.wavfile as wav
+
+(rate,sig) = wav.read("aa.wav")
+mfcc_feat = mfcc(sig,rate)
+fbank_feat = logfbank(sig,rate)
+
+print(fbank_feat[4:10,:])
